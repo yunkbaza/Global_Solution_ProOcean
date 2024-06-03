aa19a7e9e030e3c25f7746c0fd3e7b3a44c4a44c
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-06-03 11:36:29 -0300

    Modais corrigidos

--- js/script.js
+++ js/script.js
@@ -1,44 +1,68 @@
-const botaoPlataforma = document.querySelector("#botao-plataforma");
+const botaoBoia = document.querySelector("#botao-boia");
 const botaoProOcean = document.querySelector("#botao-proocean");
 const botaoNatureza = document.querySelector("#botao-natureza");
 
 
-const modalPlataforma = document.querySelector("#modal-plataforma");
+const modalBoia = document.querySelector("#modal-boia");
 const modalProOcean = document.querySelector("#modal-proocean");
 const modalNatureza = document.querySelector("#modal-natureza");
 
-botaoPlataforma.addEventListener("click" , () => {
-    modalPlataforma.showModal();
+botaoBoia.addEventListener("click" , () => {
+    modalBoia.showModal();
     document.body.classList.add('no-scroll');
+    modalBoia.style.padding = "5%";
+    modalBoia.style.display = "flex";
+    modalBoia.style.textAlign = "justify";
+    modalBoia.style.overflow = "hidden";    
 });
 
 botaoProOcean.addEventListener("click" , () => {
     modalProOcean.showModal();
     document.body.classList.add('no-scroll');
+    modalProOcean.style.padding = "5%";
+    modalProOcean.style.display = "flex";
+    modalProOcean.style.textAlign = "justify";
+    modalProOcean.style.overflow = "hidden";   
 });
 
 botaoNatureza.addEventListener("click" , () => {
     modalNatureza.showModal();
     document.body.classList.add('no-scroll');
+    modalNatureza.style.padding = "5%";
+    modalNatureza.style.display = "flex";
+    modalNatureza.style.textAlign = "justify";
+    modalNatureza.style.overflow = "hidden";   
 });
 
-const fecharPlataforma = document.querySelector("#btn-fechar-plataforma");
+const fecharBoia = document.querySelector("#btn-fechar-boia");
 const fecharProOcean = document.querySelector("#btn-fechar-proocean");
 const fecharNatureza = document.querySelector("#btn-fechar-natureza")
 
-fecharPlataforma.addEventListener("click" , () => {
-    modalPlataforma.close();
+fecharBoia.addEventListener("click" , () => {
+    modalBoia.close();
     document.body.classList.remove('no-scroll');
+    modalBoia.style.padding = "";
+    modalBoia.style.display = "";
+    modalBoia.style.textAlign = "";
+    modalBoia.style.overflow = ""
 });
 
 fecharProOcean.addEventListener("click" , () => {
     modalProOcean.close();
     document.body.classList.remove('no-scroll');
+    modalProOcean.style.padding = "";
+    modalProOcean.style.display = "";
+    modalProOcean.style.textAlign = "";
+    modalProOcean.style.overflow = ""
 });
 
 fecharNatureza.addEventListener("click" , () => {
     modalNatureza.close();
     document.body.classList.remove('no-scroll');
+    modalNatureza.style.padding = "";
+    modalNatureza.style.display = "";
+    modalNatureza.style.textAlign = "";
+    modalNatureza.style.overflow = ""
 });
 
 const fecharAoClicarFora = (modal) => {
@@ -46,10 +70,14 @@ const fecharAoClicarFora = (modal) => {
         if (event.target === modal) {
             modal.close();
             document.body.classList.remove('no-scroll');
+            modal.style.padding = "";
+            modal.style.display = "";
+            modal.style.textAlign = "";
+            modal.style.overflow = "";
         }
     });
 };
 
-fecharAoClicarFora(modalPlataforma);
+fecharAoClicarFora(modalBoia);
 fecharAoClicarFora(modalProOcean);
 fecharAoClicarFora(modalNatureza);
